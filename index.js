e37fed4951abec92f8692c4e45c445fc3e527035
Ian Gregory <justcheesy@outlook.com>
2021-06-10 03:06:21 +0100

    General commmand system, runtime-mappable keys

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 const path = require('path');
 
-const outSocket = require('unix-dgram').createSocket('unix_dgram');
+const outSocket = process.env.OUTPUT_SOCKET_PATH ? require('unix-dgram').createSocket('unix_dgram') : null;
 
 const port = process.env.PORT;
 const outSocketPath = process.env.OUTPUT_SOCKET_PATH;
@@ -16,11 +16,71 @@ app.use('/', express.static(path.join(__dirname, 'client')));
 var usernames = new Map();
 var userLastID = 1;
 
+function relay(message) {
+  if (outSocket) outSocket.send(message);
+  else console.log(`[TEST RELAY]: ${message}`);
+}
+
 io.on('connection', (socket) => {
+  function sendSystemMessage(content) {
+    socket.emit('chat message', {username: 'system', content: content});
+  }
+  const slashCommands = {
+    help: {
+      description: 'list all commands',
+      run: function() {
+        let helpText = '\n';
+        for (command in slashCommands) {
+          const args = slashCommands[command]?.args?.map(arg => `«${arg}»`).join(' ');
+          helpText += `/${command} ${args ? args + ' ' : ''}– ${slashCommands[command].description}\n`
+        }
+        sendSystemMessage(helpText);
+      }
+    },
+    nick: {
+      rawArg: true,
+      args: ['new name'],
+      description: 'set your nickname',
+      run: function(userId, newUsername) { 
+        const oldUsername = usernames.get(userId);
+        usernames.set(userId, newUsername);
+        io.emit('nickname change', {oldName: oldUsername, newName: newUsername});
+      }
+    },
+    emu: {
+      rawArg: true,
+      args: ['command'],
+      description: 'send raw wmemulator command',
+      run: function(userId, command) {
+        relay(Buffer.from(command));
+      }
+    },
+    map: {
+      args: ['key', 'input type', 'input_name'],
+      description: '(re)map key to input',
+      run: function(userId, keycode, inputType, inputName) {
+        keycodeToMessage.map(userId, keycode, [inputType, inputName]);
+        sendSystemMessage(`Mapped ${keycode} to ${inputType} ${inputName}`);
+      }
+    },
+    'map-list': {
+      description: 'list key-input mappings',
+      run: function(userId, keycode, inputType, inputName) {
+        let listing = 'Your current key-input mappings (format: key type name):\n';
+        const profile = keycodeToMessage.profile(userId);
+        for (keycode in profile) {
+          listing += `${keycode} ${profile[keycode].join(' ')}\n`;
+        }
+        sendSystemMessage(listing);
+      }
+    }
+  };
+  
   const defaultUsername = `user #${userLastID++}`;
   console.error(`user connected: ${defaultUsername}`);
   usernames.set(socket.id, defaultUsername);
   io.emit('user connected', defaultUsername);
+  sendSystemMessage('use /help to list all commands')
   
   socket.on('disconnect', () => {
     const currentUsername = usernames.get(socket.id);
@@ -29,15 +89,26 @@ io.on('connection', (socket) => {
   });
   
   socket.on('chat message', (message) => {
-    const currentUsername = usernames.get(socket.id);
-    
-    if (message.startsWith('/nick ')) {
-      message = message.substring('/nick'.length).trim();
-      const oldUsername = currentUsername;
-      usernames.set(socket.id, message);
-      io.emit('nickname change', {oldName: oldUsername, newName: message});
+    if (message.startsWith('/')) {
+      message = message.substring('/'.length).trim();
+      let spaceIndex = message.indexOf(' ');
+      const command = (spaceIndex === -1) ? message : message.substring(0, spaceIndex);
+      if (!(command in slashCommands)) {
+        sendSystemMessage('no such command; use /help to list all commands');
+        return;
+      }
+      const commandInfo = slashCommands[command];
+      message = message.substring(command.length).trim();
+      const args = commandInfo?.rawArg ? [message] : message.split(' ').filter(s => s !== '');
+      const expectedArgsCount = commandInfo?.args?.length ?? 0;
+      if (args.length !== expectedArgsCount) {
+        sendSystemMessage(`expected ${expectedArgsCount} arguments but got ${args.length}`);
+        return;
+      }
+      commandInfo.run(socket.id, ...args);
+      return;
     } else {
-      io.emit('chat message', {username: currentUsername, content: message});
+      io.emit('chat message', {username: usernames.get(socket.id), content: message});
     }
   });
   
@@ -45,30 +116,35 @@ io.on('connection', (socket) => {
 		let code = input.code;
 		if (code.startsWith("Key")) code = code.substring(3);
     
-    const converted = keycodeToMessage(code);
+    const converted = keycodeToMessage.convert(socket.id, code);
     if (converted) {
       const [eventType, eventParam] = converted;
       const eventStatus = (input.type === 'keydown') ? '1' : '0';
       const message = Buffer.from(`${eventType} ${eventStatus} ${eventParam}`);
-      outSocket.send(message);
+      relay(message);
     } else {
       console.log(`No message corresponding to key code: ${code}`);
     }
   });
   
-  outSocket.send(Buffer.from('hotplug 1 nunchuk'));
+  relay(Buffer.from('hotplug 1 nunchuk'));
 });
 
 http.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
 
-outSocket.on('error', (err) => {
-  console.error(err);
-});
-outSocket.on('connect', (err) => {
-  console.log(`Relaying to UNIX socket at ${outSocketPath}`);
-});
+if (outSocket) {
+  outSocket.on('error', (err) => {
+    console.error(err);
+  });
+  outSocket.on('connect', (err) => {
+    console.log(`Relaying to UNIX socket at ${outSocketPath}`);
+  });
 
-console.log(`Attempting to connect to UNIX socket at ${outSocketPath}`);
-outSocket.connect(outSocketPath);
+  console.log(`Attempting to connect to UNIX socket at ${outSocketPath}`);
+  outSocket.connect(outSocketPath);
+} else {
+  console.log('TEST MODE (NOT RELAYING INPUT)\nTo relay input, set env var OUTPUT_SOCKET_PATH to a (nonempty) path value');
+}
+
--- keycode_to_message.js
+++ keycode_to_message.js
@@ -1,26 +1,39 @@
-module.exports = function keycodeToMessage(keycode) {
-  switch (keycode) {
-  case 'W': return ['button', 'WIIMOTE_1'];
-  case 'Q': return ['button', 'WIIMOTE_2'];
-  case 'Tab': return ['button', 'WIIMOTE_A'];
-  case 'E': return ['button', 'WIIMOTE_B'];
-  case 'ArrowUp': return ['button', 'WIIMOTE_RIGHT'];
-  case 'ArrowDown': return ['button', 'WIIMOTE_LEFT'];
-  case 'ArrowLeft': return ['button', 'WIIMOTE_UP'];
-  case 'ArrowRight': return ['button', 'WIIMOTE_DOWN'];
-  
-  case 'H': return ['button', 'HOME'];
-  case 'P': return ['button', 'WIIMOTE_PLUS'];
-  case 'O': return ['button', 'WIIMOTE_MINUS'];
-  
-  case 'N': return ['button', 'WIIMOTE_A'];
-  case 'B': return ['button', 'WIIMOTE_B'];
-  case 'Digit1': return ['button', 'WIIMOTE_1'];
-  case 'Digit2': return ['button', 'WIIMOTE_2'];
-  case 'BracketLeft': return ['analog_motion', 'STEER_LEFT'];
-  case 'BracketRight': return ['analog_motion', 'STEER_RIGHT'];
-  
-  case 'F4': return ['emulator_control', 'power_off'];
-  default: return null;
+const defaultProfile = {
+  'W': ['button', 'WIIMOTE_1'],
+  'Q': ['button', 'WIIMOTE_2'],
+  'Tab': ['button', 'WIIMOTE_A'],
+  'E': ['button', 'WIIMOTE_B'],
+  'ArrowUp': ['button', 'WIIMOTE_RIGHT'],
+  'ArrowDown': ['button', 'WIIMOTE_LEFT'],
+  'ArrowLeft': ['button', 'WIIMOTE_UP'],
+  'ArrowRight': ['button', 'WIIMOTE_DOWN'],
+
+  'H': ['button', 'HOME'],
+  'P': ['button', 'WIIMOTE_PLUS'],
+  'O': ['button', 'WIIMOTE_MINUS'],
+  'N': ['button', 'WIIMOTE_A'],
+  'B': ['button', 'WIIMOTE_B'],
+
+  'Digit1': ['button', 'WIIMOTE_1'],
+  'Digit2': ['button', 'WIIMOTE_2'],
+  'BracketLeft': ['analog_motion', 'STEER_LEFT'],
+  'BracketRight': ['analog_motion', 'STEER_RIGHT'],
+  'F4': ['emulator_control', 'power_off'],
+};
+
+module.exports = {
+  profiles: new Map(),
+  profile(userId) {
+    const profile = this.profiles.get(userId);
+    if (profile) return profile;
+    this.profiles.set(userId, defaultProfile);
+    return defaultProfile;
+  },
+  convert(userId, keycode) {
+    return this.profile(userId)[keycode];
+  },
+  map(userId, keycode, commandComponents) {
+    this.profile(userId)[keycode] = commandComponents;
   }
 };
+
